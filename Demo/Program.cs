5840ee3221e0b558461e236c5eff801522afbb21
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2024-12-22 11:05:43 +0200

    02 Nullable Value Type

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -31,6 +31,63 @@ namespace Demo
             // Explicit Casting
             // unsafe casting (may throw or making exception)
             #endregion
+            #region Nullable Value Type
+            //int X = null; ==>xxxxxx not valid
+            //         <===Nullable integer===>
+            // nullable value type ===>value type allow null as valid value.
+            //int? age = 20; // nullable<int> allow int value or null as valid value
+            //age = null;
+            //double? salary = 4000.5;//allow double value or null as valid value
+            //salary = null;
+            //int x = 6;
+            //int? y = 7;nullable<int>
+            //int x = 4;
+            //int? y = 5;
+            //y ==> nullable <int> allow int we null
+            //y = x;//implicit casting 
+            //int? x = 5;
+            //x = null;
+            // int y;
+            //int y = x;// y allow int only
+            //int y =(int) x;// Explicit casting as i forced el x to be from type int , may throw exception.
+            // Devensive Code
+            //if (x!=null)
+            //{
+            //     y = (int)x;
+            //}
+            //else
+            //{
+            //    y = 0;
+            //}
+            //Console.WriteLine(y);
+            //=============================
+            //if (x is not null)
+            //{
+            //    y = (int)x;
+            //}
+            //else
+            //{
+            //    y = 0;
+            //}
+            //=============================
+            //if (x.HasValue)
+            //{
+            //    y = x.Value;
+            //}
+            //else
+            //{
+            //    y = 0;
+            //}
+            //============================
+            //y = x.HasValue? x.Value:0 ;
+            //y = x is not null ? x.Value : 0;
+            //============================
+            //     Null Coalescing Operator ==>??
+            // y = x ?? 0; // syntax sugar from y = x is not null ? x.Value : 0;
+           // Console.WriteLine(y);
+
+            #endregion
+
 
         }
     }
